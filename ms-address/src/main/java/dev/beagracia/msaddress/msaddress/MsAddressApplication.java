95a4f93f44ded667d5008418779dad554b4c8851
whathebea <bea.graciast@gmail.com>
2023-01-24 21:54:20 -0300

    added starter project

---
+++ ms-address/src/main/java/dev/beagracia/msaddress/msaddress/MsAddressApplication.java
@@ -0,0 +1,13 @@
+package dev.beagracia.msaddress.msaddress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAddressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAddressApplication.class, args);
+	}
+
+}
---
+++ ms-address/src/test/java/dev/beagracia/msaddress/msaddress/MsAddressApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.beagracia.msaddress.msaddress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAddressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
